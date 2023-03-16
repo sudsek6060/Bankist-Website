2ca14c3fff39b72762e6bfc49067e5d0ec4fecda
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-16 21:53:42 +0530

    open account window

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
 const allSections = document.querySelectorAll('.section');
 const imgTargets = document.querySelectorAll('img[data-src]');
+
+// Modal Window
+const openModal = function(e){
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+};
+
+const closeModal = function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
