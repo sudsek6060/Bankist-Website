f0d68f15b5318906813a0928eadec7855c58e2ed
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-17 12:01:54 +0530

    lazy loding img

--- script.js
+++ script.js
@@ -100,3 +100,24 @@ allSections.forEach(function (section){
     sectionObserver.observe(section);
     section.classList.add('section--hidden')
 });
+
+// Lazy Loading Images
+const loadImg = function (entries, observer) {
+    const [ entry ] = entries;
+
+    if(!entry.isIntersecting) return;
+
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function() {
+        entry.target.classList.remove('lazy-img');
+    });
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0.15,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img))
