0e8a0354e16a6944185d6c66f2050bf4ced01c14
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-17 11:52:46 +0530

    reveal section

--- script.js
+++ script.js
@@ -80,3 +80,23 @@ window.addEventListener('scroll', function () {
     if(window.scrollY > initialCoords.top) nav.classList.add('sticky');
     else nav.classList.remove('sticky')
 });
+
+// Reveal Section
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+
+    if(!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(function (section){
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden')
+});
