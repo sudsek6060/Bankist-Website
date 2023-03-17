bb3af7acdd356cdbc48c54d599ff0d91424e0eae
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-17 12:21:35 +0530

    slider functions

--- script.js
+++ script.js
@@ -135,3 +135,72 @@ tabsContainer.addEventListener('click', function(e){
 
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
 });
+
+// Slider
+const slider = function () {
+    const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const createDots = function () {
+    slides.forEach(function(_, i){
+        dotContainer.insertAdjacentElement(
+            'beforeend',
+            `<button class="dots__dot" data-slide="${i}"></button>`
+        );
+    });
+  };
+
+  const activeDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+// Next Slide 
+  const nextSlide = function () {
+    if(curSlide === maxSlide -1){
+        curSlide = 0;
+    }else {
+        curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+
+//   Previous Slide
+const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  
+}
