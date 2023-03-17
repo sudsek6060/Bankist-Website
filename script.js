dcce8736379f21a3e2707b085578562d778a89a3
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-17 12:10:43 +0530

    tabbed components

--- script.js
+++ script.js
@@ -120,4 +120,18 @@ const imgObserver = new IntersectionObserver(loadImg, {
     threshold: 0.15,
 });
 
-imgTargets.forEach(img => imgObserver.observe(img))
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// // Tabbed Components
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab');
+
+  if(!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+
+  clicked.classList.add('operations__tab--active');
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+});
